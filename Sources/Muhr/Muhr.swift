44d4d484fa6260e6d3a3ae943d3bb81f74e5a235
Muhammad <muhammad.tohirov@fido.uz>
2026-01-27 16:02:13 +0500

    init: Muhr Swift Package yaratildi
    - Package nomi: Muhr (O'zbekcha: Muhr/Pechat)
    - Minimal iOS 14, macOS 11 platformalar
    - Asosiy Package.swift manifest
    - Entry point: Muhr.swift

--- Sources/Muhr/Muhr.swift
+++ Sources/Muhr/Muhr.swift
@@ -1,2 +1,34 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Muhr.swift
+//  Muhr
+//
+//  Created by Muhammad on 27/01/26.
+//
+
+/// Muhr - Xavfsizlik kutubxonasi
+///
+/// Raqamli imzo (ERI/ЭЦП) va shifrlash funksionalligi.
+///
+/// ## Imkoniyatlar:
+/// - X.509 sertifikat bilan imzolash
+/// - Ma'lumotlarni shifrlash
+/// - Keychain integratsiyasi
+///
+/// ## Foydalanish:
+/// ```swift
+/// import Muhr
+///
+/// // Imzolash
+/// let signature = try await Muhr.sign(data)
+///
+/// // Shifrlash (kelajakda)
+/// let encrypted = try Muhr.encrypt(data)
+/// ```
+public enum Muhr {
+
+    /// Kutubxona versiyasi
+    public static let version = "1.0.0"
+
+    /// Build raqami
+    public static let build = 1
+}
